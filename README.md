84a3f783a267b2b182d4606b1454cb9e50326937
Samuel Schwob <samuel.schwob@naomi.fr>
2024-05-23 09:01:49 +0200

    Initial commit with template from https://github.com/sschwob/rails-template

---
+++ README.md
@@ -0,0 +1,1 @@
+Rails app generated with [sschwob/rails-template](https://github.com/sschwob/rails-template)
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  # TODO : Remove comment after create devise user model
+  # before_action :authenticate_user!
+end
