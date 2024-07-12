0c6081999f18bab5e66d91534e9921bdfdb165ad
Samuel Schwob <samuel.schwob@naomi.fr>
2024-07-12 16:04:42 +0200

    patch notes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @last_contents = Content.where(enable: true).order(created_at: :desc).limit(10)
   end
 end
